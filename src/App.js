ea507764697e28d6cf95d28bfcda160945cfd9ac
ksb9242 <ksb9242@gmail.com>
2020-10-06 02:36:01 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//실질적으로 웹 화면을 담당하는 부분
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>Let's develop management system!</h2>
+        
     </div>
   );
 }
