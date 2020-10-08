ce2742ac7a64434d9c59e7b63b5811f309ff7122
ksb9242 <ksb9242@gmail.com>
2020-10-08 23:08:48 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,37 +18,28 @@ const styles = theme => ({
   table : {
     minWidth : 1080
   }
-})
+});
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'http://placeimg.com/64/64/1',
-  'name' : '나동빈',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'http://placeimg.com/64/64/2',
-  'name' : '홍길동',
-  'birthday' : '960355',
-  'gender' : '남자',
-  'job' : '군인'
-},
-{
-  'id' : 3,
-  'image' : 'http://placeimg.com/64/64/3',
-  'name' : '이순신',
-  'birthday' : '920312',
-  'gender' : '여자',
-  'job' : '디자이너'
-}
-]
 
 //실질적으로 웹 화면을 담당하는 부분
 class App extends Component {
+
+  state = {
+    customers : ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -66,7 +57,7 @@ class App extends Component {
             </TableHead>
             <TableBody>
               {
-                customers.map(c => {
+                this.state.customers ? this.state.customers.map(c => {
                   return (
                     <Customer
                       key={c.id}
@@ -78,8 +69,7 @@ class App extends Component {
                       job={c.job}
                       />
                   );
-                })
-              }
+                }) : ""}
   
             </TableBody>
           </Table>
